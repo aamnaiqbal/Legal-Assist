2844868f18d246d606fcc9a25ab6875547566a3e
Aamna <your-email@example.com>
2024-10-17 13:39:58 +0500

    adminModel

--- server/Models/adminModel.js
+++ server/Models/adminModel.js
@@ -5,16 +5,20 @@ const adminSchema= new mongoose.Schema({
     name:{
         type: String,
         required: [true, "Please enter your name"],
-        trim: true
-
+        trim: true,
+        validate: {
+            validator: function(val){
+                return /^[a-zA-Z\s]+$/.test(val)
+            },
+            message: "Name must contain only letters and spaces."
+        }
     },
     email:{
         type: String,
         unique: true,
         required: [true, "Please enter your email. It is required field"],
-        validate: [validate.isEmail, "Please enter valid email"],
+        validate: [validator.isEmail, "Please enter valid email"],
         lowercase: true,
-        
     },
     password:{
         type: String,
@@ -32,9 +36,36 @@ const adminSchema= new mongoose.Schema({
         }
     },
     cnicNo:{
-
+        type: Number,
+        required: [true, "CNIC No. is a required field."],
+        unique: true,
+        validate: {
+            validator:function(val){
+                return val.toString().length===13
+            }, 
+            message: "CNIC must contain exactly 13 digits without any dashes"
+        }
     },
+    licenseNo:{
+        type: Number,
+        required: [true, "License No. is a required field."],
+        unique: true,
+        validate: {
+            validator:function(val){
+                return val.toString().length===10
+            }, 
+            message: "License number must contain exactly 10 digits."
+        }
+    },
+    legalExpertise:{
+        type: String,
+        enum: ["Property Law", "Family Law", "Criminal Law"],
+        required: [true, "The legal expertise must be selected."]
+    },
+    profileImg:{
+        type: String,
+    }
     
 });
 
-const Admin= mongoose.Model('Admin', 'adminSchema')
+const Admin= mongoose.Model('Admin', adminSchema)
